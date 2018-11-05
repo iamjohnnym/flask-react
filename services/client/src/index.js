3efdf79c69b419901abc0f11b9bc687d1b2df55f
John Martin <johnny@iamjohnnym.com>
2018-11-04 20:53:22 -0800

    merge react/test
    AddUser view and tests
    Moved userList to UsersList
    Implemented snapshots for testing ui output

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
-import UsersList from './components/userList';
+import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-        users: []
+        users: [],
+        username: '',
+        email: ''
     };
+    this.addUser = this.addUser.bind(this);
   };
   componentDidMount() {
     this.getUsers();
   };
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { this.setState({ users: res.data.data.users }); })
-    .catch((err) => { console.log(err); });
+    .then((response) => { this.setState({ users: response.data.data.users }); })
+    .catch((error) => { console.log(error); });
+  };
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((response) => {
+      this.getUsers();
+      this.setState({ username: '', email: ''});
+     })
+    .catch((error) => { console.log(error); });
   };
   render() {
     return (
@@ -27,6 +44,13 @@ class App extends Component {
             <div className="column is-one-third">
               <br/>
               <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <hr/><br/>
               <UsersList users={this.state.users}/>
             </div>
