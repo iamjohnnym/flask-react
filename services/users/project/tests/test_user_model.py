17efb8f9ca30346702b5879b52fbfef142d4a19f
John Martin <johnny@iamjohnnym.com>
2018-11-07 22:53:53 -0800

    * tests max length of User.email and User.username

--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -2,7 +2,7 @@ import unittest
 from project import db
 from project.api.models import User
 from project.tests.base import BaseTestCase
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, DataError
 
 from test_users import add_user
 
@@ -41,6 +41,29 @@ class TestUserModel(BaseTestCase):
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
+
+    def test_add_user_max_length_username(self):
+        user = User(
+            username='this_is_my_super_duper_long_username',
+            email='test.user@test_example.com'
+        )
+        db.session.add(user)
+        self.assertRaises(DataError, db.session.commit)
+
+    def test_add_user_max_length_email(self):
+        user = User(
+            username='test.user',
+            # this is the dumbest standard ever, 254 characters as a max
+            # possible length? Come on man.
+            email='this_is_my_super_incredibly_obnoxiously_long_email_' + \
+                  'prefix_that_goes_on_and_on_my_friends_bang_pow_wow_bow' + \
+                  'how_bou_nah_bah_super_calafragalisticespialadocious' + \
+                  '@how.much.more.do.i.have.to.type.to.reach.this' + \
+                  '.rediculous.length.to.fail.this.two.hundred.and.fifty' + \
+                  'four.character.length.com'
+        )
+        db.session.add(user)
+        self.assertRaises(DataError, db.session.commit)
 
     def test_model_to_json(self):
         user = add_user(
