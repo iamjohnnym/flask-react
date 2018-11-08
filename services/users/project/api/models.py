b7e93d9f76289c7d940c012d2b21d9d8139597ea
John Martin <johnny@iamjohnnym.com>
2018-11-07 22:30:04 -0800

    * User.email length has a max length of 254 characters * User.username has a max lenght of 32

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False, unique=True)
-    email = db.Column(db.String(128), nullable=False, unique=True)
+    username = db.Column(db.String(32), nullable=False, unique=True)
+    email = db.Column(db.String(254), nullable=False, unique=True)
     active = db.Column(db.Boolean(), default=True, nullable=False)
     created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
 
